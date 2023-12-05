8f11768c37ffa40228e9001f506ea4cef35e192d
MarleneSalas <201g0614@rcarbonifera.tecnm.mx>
2023-12-05 14:04:08 -0600

    Vista producto, estilos terminados

---
+++ AlkaShoes/Areas/Admin/Controllers/ProductosController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlkaShoes.Areas.Admin.Controllers
+{
+    public class ProductosController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Eliminar()
+        {
+            return View();
+        }
+
+    }
+}
